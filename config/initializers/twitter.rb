7e42a64bce7ebe5d2024cfaa725e3aa510200d1a
Lucille James <lucille.james00@gmail.com>
2020-09-15 14:45:24 +0200

    added Twitter config variables

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -5,6 +5,5 @@ $twitter_client = Twitter::REST::Client.new(
       :consumer_key       => YOUR_CONSUMER_KEY,
       :consumer_secret    => TWITTER_SECRET_KEY,
       :access_token       => TWITTER_TOKEN,
-      # :access_token_secret=> "",
     }       
  )
